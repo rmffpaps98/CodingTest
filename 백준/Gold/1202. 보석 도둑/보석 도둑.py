9716328ffe8b066cb3e849ab820fff87700dd4ce
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2023-10-09 01:46:25 +0900

    Update 보석 도둑.py

--- 백준/Gold/1202. 보석 도둑/보석 도둑.py
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -20,3 +20,28 @@ for i in max_weight :
         result -= heapq.heappop(tmp)
 
 print(result)
+
+#################################################################
+# [최초 풀이 코드]  >> 시간초과로 인한 실패
+# import sys
+
+# input = sys.stdin.readline
+
+# n, k = map(int, input().split())
+
+# jewel = [list(map(int, input().split())) for _ in range(n)]
+# max_weight = [int(input()) for _ in range(k)]
+# result = 0
+
+# jewel.sort(reverse=True, key=lambda x :x[1])
+
+# while len(max_weight) > 0 :
+#     weight = max_weight.pop(0)
+#     for i in jewel :
+#         if weight >= i[0]:
+#             result += i[1]
+#             jewel.remove(i)
+#             break
+
+# print(result)
+##################################################################
