8c72de3b59825aa529eb6f92cc78b5848b3eb6f2
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2023-10-18 14:37:40 +0900

    저울.py

--- 백준/Gold/2437. 저울/저울.py
+++ 백준/Gold/2437. 저울/저울.py
@@ -11,3 +11,26 @@ for w in weight:
         break
 
 print(answer)
+
+
+##################################################
+# 최초 풀이 답안 : 시간복잡도가 높아 비효율적
+# n = int(input())
+
+# weight = list(map(int, input().split()))
+# weight.sort(reverse=True)
+
+# answer = 0
+
+# for i in range(1, sum(weight)+1) :
+#     num = i
+#     for j in weight :
+#         if num >= j :
+#             num -= j
+    
+#     if num > 0 :
+#         answer = i
+#         break
+
+# print(answer)
+##################################################
