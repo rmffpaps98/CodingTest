1b0e944b69d9322878786afc5c59f89917c30baa
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2024-02-16 18:22:02 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,2 +1,2 @@
 def solution(rsp):
-    return ''.join([str(2) if i == str(5) else str(0) if i == str(2) else str(5) for i in rsp])
+    return ''.join(["2" if i == "5" else "0" if i == "2" else "5" for i in rsp])
