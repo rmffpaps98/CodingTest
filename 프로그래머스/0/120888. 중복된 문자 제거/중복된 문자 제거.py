046161aa03ec99b5936b74ac47dba516f6018de3
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2024-02-17 05:30:49 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.py
@@ -0,0 +1,8 @@
+def solution(my_string):
+    answer = ''
+    for i in my_string :
+        if i in answer:
+            pass
+        else :
+            answer = answer + i        
+    return answer
