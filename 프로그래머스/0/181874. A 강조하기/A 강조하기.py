b4c50391f3d578639258432677af6ae05bf0b9c8
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2024-02-22 04:53:05 +0900

    [level 0] Title: A 강조하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.py
@@ -0,0 +1,7 @@
+def solution(myString):
+    answer = ''
+    myString = myString.replace('a', 'A')
+    for i in myString :
+        if i != 'A' : answer += i.lower()
+        else : answer += i
+    return answer
