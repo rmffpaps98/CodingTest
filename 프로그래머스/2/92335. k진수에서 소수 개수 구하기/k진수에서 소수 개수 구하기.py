e9390f96ecccfe448a5e07d4de5fbd9284cde52a
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2024-03-13 03:56:35 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -1,26 +1,19 @@
 def solution(n, k):
     answer = 0
-    k_num = to_k_number(n, k)
+    t = ''
     
-    for n in k_num.split('0') :
-        if n == "" : continue
-        if is_prime_num(int(n)) :
-            answer += 1
+    while n > 0:
+        t += str(n % k)
+        n //= k
+    t = ''.join(reversed(t))
+
+    for i in t.split('0'):
+        if i == '':
+            continue
+        for j in range(2,int(int(i) ** (1/2)) + 1):
+            if int(i) % j == 0:
+                break
+        else:
+            if i != '1':
+                answer += 1
     return answer
-
- 
-
-def to_k_number(n, k) :
-    ret = ""
-    while n > 0 :
-        ret += str(n % k)
-        n = n //  k
-    return ''.join(reversed(ret))
-
-def is_prime_num(k) :
-    if k == 2 or k == 3 : return True
-    if k % 2 == 0 or k < 2 : return False
-    for i in range(3, int(k ** 0.5) + 1, 2) :
-        if k % i == 0:
-            return False
-    return True
