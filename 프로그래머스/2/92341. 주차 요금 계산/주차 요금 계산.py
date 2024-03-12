9a9788e8dca7096084942f48681dc2088bee6342
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2024-03-13 06:12:32 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -2,36 +2,29 @@ import math
 
 def solution(fees, records):
     answer = []
-    dict_rec = {}
-    dict_time = {}
-    bhour, bfee, fmin, pfee = map(int, fees)
+    bt, bf, pm, pf = fees
+    d = {}
+    mt = 23*60 + 59
     
-    for record in records:
-        time, num, ent = record.split()
-        hour, mn = map(int, time.split(':'))
-        minutes = (hour * 60) + mn
-        
-        if num not in dict_rec and ent == "IN":
-            dict_rec[num] = minutes
-        elif num in dict_rec and ent == "OUT" :
-            if num not in dict_time :
-                dict_time[num] = 0
-            dict_time[num] += minutes - dict_rec[num]
-            del dict_rec[num]
-            
-    if dict_rec :        
-        for num in dict_rec :
-            if num not in dict_time :
-                dict_time[num] = 0
-            dict_time[num] += 1439 - dict_rec[num]
-            
-    dict_time = sorted(dict_time.items())
-    for _, value in dict_time :
-        if value > bhour :
-            fee = bfee + (math.ceil((value - bhour) / fmin) * pfee)
+    for i in records :
+        t, n, e = i.split()
+        h, m = t.split(':')
+        time = int(h)*60 + int(m)
+        if e == 'IN' :
+            d[n] = d.get(n, 0) + time
         else :
-            fee = bfee
+            d[n] -= time
             
-        answer.append(fee)
+    for i, j in d.items():
+        if j >= 0 :
+            d[i] -= mt
+        
+    d = sorted(d.items(), key=lambda x:x[0])
+    
+    for _, j in d :
+        if abs(j) > bt :
+            answer.append(bf + math.ceil((abs(j) - bt) / pm) * pf)
+        else :
+            answer.append(bf)
         
     return answer
