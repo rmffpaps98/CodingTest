eeb062cfd3709f195a249d87a97a0fdcdfbb2c71
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2024-09-12 03:06:25 +0900

    [level 2] Title: 올바른 괄호, Time: 6.48 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,11 +1,13 @@
 def solution(s):
     s1 = []
+    
     for i in s :
-        if i == '(' :
+        if i == "(" :
             s1.append(i)
         else :
             if s1 :
                 s1.pop()
             else :
                 s1.append(i)
-    return True if not s1 else False
+            
+    return False if s1 else True
