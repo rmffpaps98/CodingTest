8d4a3850cd74297af9391b9609abb75e34828c10
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2024-09-12 02:45:21 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.06 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,3 +1,2 @@
 def solution(s):
-    s = list(map(int, s.split()))
-    return ' '.join([str(min(s)), str(max(s))])
+    return "{} {}".format(min(list(map(int, s.split()))), max(list(map(int, s.split()))))
