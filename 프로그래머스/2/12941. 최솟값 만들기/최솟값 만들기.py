23d37e6f9797d99f33194a8083727d74acd053bd
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2024-09-12 03:31:19 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.42 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -1,2 +1,7 @@
 def solution(A,B):
-    return sum([i*j for i, j in zip(sorted(A), sorted(B, reverse=True))])
+    answer = 0
+
+    for i, j in zip(sorted(A), sorted(B, reverse=True)) :
+        answer += i*j
+
+    return answer
