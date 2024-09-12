bd38da180b985a93389094f289facfcd9200b09d
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2024-09-13 04:03:12 +0900

    [level 2] Title: 카펫, Time: 1.13 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,7 +1,7 @@
 def solution(brown, yellow):
-    for i in range(1, int(yellow**0.5) + 1) :
-        if yellow % i == 0 :
-            w = yellow // i
-            h = i
-            if 2 * (w+h) + 4 == brown :
-                return [w+2, h+2]
+    total = brown + yellow
+    for height in range(1, int(total**0.5) + 1):
+        if total % height == 0:
+            width = total // height
+            if (width - 2) * (height - 2) == yellow:
+                return [width, height]
