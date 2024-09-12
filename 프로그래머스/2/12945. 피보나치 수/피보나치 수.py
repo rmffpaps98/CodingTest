33a053e783bd8bf08fb9c2c390c5598b5797ad1b
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2024-09-13 02:55:43 +0900

    [level 2] Title: 피보나치 수, Time: 83.14 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,8 +1,6 @@
 def solution(n):
-    answer = 0
-    f1 = 0
-    f2 = 1
-    for i in range(2, n+1) :
-        answer = f1 + f2
-        f1, f2 = f2, answer
-    return answer % 1234567
+    p, c = 0, 1
+    for _ in range(2, n + 1):
+        p, c = c, p + c
+
+    return c % 1234567
