10f8684eecb65b6ee55d62d22c82616d58437d92
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2024-09-19 17:01:26 +0900

    [level 2] Title: 귤 고르기, Time: 62.78 ms, Memory: 26.4 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,19 +1,20 @@
 def solution(k, tangerine):
     answer = 0
-    t = {}
     
+    tan_d = {}
     for i in tangerine:
-        if i in t:
-            t[i] += 1
+        if i in tan_d:
+            tan_d[i] += 1
         else:
-            t[i] = 1
-            
-    t = sorted(t.values(), reverse=True)
+            tan_d[i] = 1
     
-    for i in t:
-        answer += 1
-        k -= i
-        if k <= 0:
+    tan_d = sorted(tan_d.items(), key=lambda x: x[1], reverse=True)
+    
+    for size, count in tan_d:
+        if k > 0:
+            k -= count
+            answer += 1
+        else :
             break
-            
+    
     return answer
