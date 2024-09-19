47fd4692924629ce51db6fb9ce0a116c034e801b
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2024-09-19 14:23:59 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.22 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,7 +1,8 @@
 def solution(n):
     if n < 3 : return n
-    
+
     a, b = 1, 2
-    for i in range(3,n+1):
-        a, b = b, a+b
+    for i in range(3, n+1) :
+        a, b = b, a + b
+        
     return b % 1234567
