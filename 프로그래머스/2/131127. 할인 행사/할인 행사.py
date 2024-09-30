d9be1869c3185aaf7704b43e86503c347d7b4e06
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2024-10-01 01:04:02 +0900

    [level 2] Title: 할인 행사, Time: 46.29 ms, Memory: 17.3 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,10 +1,30 @@
 def solution(want, number, discount):
     answer = 0
-    n = len(discount)
     wn = {i : j for i, j in zip(want, number)}
-    for i in range(n - 9) :
-        s = discount[i:i+10]
-        sn = {i : s.count(i) for i in s}
-        if wn == sn :
+    n = len(discount)
+    
+    current_window = {}
+    for i in discount[:10] :
+        if i in current_window :
+            current_window[i] += 1
+        else :
+            current_window[i] = 1
+            
+    if current_window == wn :
+        answer += 1
+        
+    for i in range(10, n) :
+        if discount[i - 10] in current_window :
+            current_window[discount[i - 10]] -= 1
+            if current_window[discount[i - 10]] == 0 :
+                del current_window[discount[i - 10]]
+                
+        if discount[i] in current_window :
+            current_window[discount[i]] += 1
+        else :
+            current_window[discount[i]] = 1
+            
+        if current_window == wn:
             answer += 1
+            
     return answer
