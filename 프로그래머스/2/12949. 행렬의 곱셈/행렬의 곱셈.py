1d6a458cc9a9ef76cebd69282a9bbdf0c3708320
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2024-10-05 03:05:14 +0900

    [level 2] Title: 행렬의 곱셈, Time: 72.64 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,13 +1,2 @@
 def solution(arr1, arr2):
-    answer = []
-    
-    for i in range(len(arr1)):
-        arr = []
-        for j in range(len(arr2[0])):
-            tmp = 0
-            for k in range(len(arr1[0])):
-                tmp += arr1[i][k] * arr2[k][j]
-            arr.append(tmp)
-        answer.append(arr)
-            
-    return answer
+    return [[sum(arr1[i][k] * arr2[k][j] for k in range(len(arr1[0]))) for j in range(len(arr2[0]))] for i in range(len(arr1))]
