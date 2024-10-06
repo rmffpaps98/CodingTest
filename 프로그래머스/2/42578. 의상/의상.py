c00e42726a6984018a23c207371bb830df9f9a77
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2024-10-07 01:59:08 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,11 +1,14 @@
 def solution(clothes):
     answer = 1
-    clo = {}
     
-    for _, i in clothes :
-        clo[i] = clo.get(i, 0) + 1
-             
-    for i in clo :
-        answer *= clo[i] + 1
+    cloth = {}
+    for i, j in clothes :
+        if j in cloth :
+            cloth[j] += 1
+        else :
+            cloth[j] = 1
+    
+    for i in cloth.values() :
+        answer *= (i + 1)
         
     return answer - 1
