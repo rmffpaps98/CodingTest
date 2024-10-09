78e38da7268c67ff9aadff212eb7d63dad9a2e35
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2024-10-10 02:19:03 +0900

    [level 2] Title: 기능개발, Time: 0.47 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,23 +1,16 @@
 def solution(progresses, speeds):
     answer = []
-    day = []
     
-    for i, j in zip(progresses, speeds) :
-        d = 0
-        while i < 100 :
-            d += 1
-            i += j
-        day.append(d)
+    while progresses:
+        progresses = [progresses[i] + speeds[i] for i in range(len(progresses))]
         
-    prev = day[0]
-    c = 1
-    for i in day[1:] :
-        if i <= prev :
-            c += 1
-        else :
-            answer.append(c)
-            c = 1
-            prev = i
-            
-    answer.append(c)
+        cnt = 0
+        while progresses and progresses[0] >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            cnt += 1
+        
+        if cnt > 0:
+            answer.append(cnt)
+    
     return answer
