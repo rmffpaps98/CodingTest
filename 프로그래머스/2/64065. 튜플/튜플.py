974370bec4a375fc9c2d687e7644dc35bc73c5cf
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2024-10-11 06:31:26 +0900

    [level 2] Title: 튜플, Time: 81.63 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -2,12 +2,15 @@ def solution(s):
     dic = {}
     tmp = ''
     
-    for i in s :
-        if i.isdigit() :
+    for i in s:
+        if i.isdigit():
             tmp += i
-        else :
-            if tmp.isdigit() :
+        else:
+            if tmp:
                 dic[tmp] = dic.get(tmp, 0) + 1
                 tmp = ''
-            
+    
+    if tmp:
+        dic[tmp] = dic.get(tmp, 0) + 1
+    
     return [int(num) for num in sorted(dic, key=lambda x: dic[x], reverse=True)]
