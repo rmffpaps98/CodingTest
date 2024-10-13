09979ff82b5f0264b90233ba41f25d979dc53e97
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2024-10-13 09:26:14 +0900

    [level 2] Title: 피로도, Time: 46.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,18 +1,14 @@
-answer = 0
-
-def dfs(k, cnt, d, v) :
-    global answer
-    if cnt > answer :
-        answer = cnt
-        
-    for i in range(len(d)) :
-        if not v[i] and k >= d[i][0] :
+def dfs(k, cnt, d, v, answer):
+    answer = max(answer, cnt)
+    
+    for i in range(len(d)):
+        if not v[i] and k >= d[i][0]:
             v[i] = True
-            dfs(k - d[i][1], cnt+1, d, v)
+            answer = dfs(k - d[i][1], cnt + 1, d, v, answer)
             v[i] = False
-            
-def solution(k, dungeons):
-    global answer
-    visited = [False] * len(dungeons)
-    dfs(k, 0, dungeons, visited)
+    
     return answer
+
+def solution(k, dungeons):
+    visited = [False] * len(dungeons)
+    return dfs(k, 0, dungeons, visited, 0)
