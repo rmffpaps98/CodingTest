95748a38fa9fdf677f17e42dcf9950022771788c
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2024-10-14 20:08:39 +0900

    [level 2] Title: 모음 사전, Time: 1.60 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,27 +1,19 @@
-def solution(word):
-    answer = 0
-    w = ['A', 'E', 'I', 'O', 'U']
-    idx = 0
-    
-    for i in range(len(w)) :
-        idx += 1
-        if w[i] == word :
-            return idx
-        for j in range(len(w)) :
-            idx += 1
-            if w[i]+w[j] == word :
-                return idx
-            for k in range(len(w)) :
-                idx += 1
-                if w[i]+w[j]+w[k] == word :
-                    return idx
-                for l in range(len(w)) :
-                    idx += 1
-                    if w[i]+w[j]+w[k]+w[l] == word :
-                        return idx
-                    for m in range(len(w)) :
-                        idx += 1
-                        if w[i]+w[j]+w[k]+w[l]+w[m] == word :
-                            return idx
+def dfs(current_word, word, w, answer):
+    if current_word == word:
+        return answer[0]
 
-    return answer
+    if len(current_word) == 5:
+        return None
+
+    for vowel in w:
+        answer[0] += 1
+        result = dfs(current_word + vowel, word, w, answer)
+        if result is not None:
+            return result
+
+def solution(word):
+    w = ['A', 'E', 'I', 'O', 'U']
+    answer = [0]
+    
+
+    return dfs('', word, w, answer)
