6e7c189f221ea13fdeca78f7e02cb4aaac566bf3
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2024-10-15 05:24:04 +0900

    [level 3] Title: 네트워크, Time: 0.68 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,17 +1,16 @@
+def dfs (node, visited, computers, n) :
+    visited[node] = True
+    for con in range(n) :
+        if con != node and computers[node][con] == 1 and not visited[con] :
+            dfs(con, visited, computers, n)
+
 def solution(n, computers):
     answer = 0
-    visited = [0] * n
-
-    for i in range(n):
-        if not visited[i]:
-            stack = [i]
-            while stack:
-                node = stack.pop()
-                if not visited[node]:
-                    visited[node] = True
-                    for connect in range(n):
-                        if connect != node and computers[node][connect] == 1 and not visited[connect]:
-                            stack.append(connect)
+    visited = [False] * n
+    
+    for i in range(n) :
+        if not visited[i] :
+            dfs(i, visited, computers, n)
             answer += 1
-
+            
     return answer
