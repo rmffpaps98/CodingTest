a30649959b923d08e89076cad80abf2598f20503
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2024-10-17 05:40:57 +0900

    [level 2] Title: 프로세스, Time: 0.76 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,12 +1,13 @@
 def solution(priorities, location):
     answer = 0
-    q = [(i,j) for i, j in enumerate(priorities)]
+    a = [(idx, i) for idx, i in enumerate(priorities)]
     
-    while True :
-        c = q.pop(0)
-        if any(c[1] < i[1] for i in q) :
-            q.append(c)
-        else :
+    while True:
+        c = a.pop(0)
+        
+        if any(c[1] < i[1] for i in a):
+            a.append(c)
+        else:
             answer += 1
-            if c[0] == location :
+            if c[0] == location:
                 return answer
