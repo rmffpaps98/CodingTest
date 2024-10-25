a2139bc520acca5c87100e7420e8be039018a718
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2024-10-25 20:34:39 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+if n == 0:
+    print(0)
+elif n == 1:
+    print(1)
+else:
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    dp[2] = 3
+    for i in range(3, n + 1):
+        dp[i] = (dp[i-1] + dp[i-2] + dp[i-2]) % 10007
+    print(dp[n])
