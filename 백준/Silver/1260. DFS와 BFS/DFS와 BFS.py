eb61e4d84707923ad5d4157770aa27f312d32ebf
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2024-11-06 20:41:14 +0900

    [Silver II] Title: DFS와 BFS, Time: 300 ms, Memory: 32300 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,35 +1,33 @@
-from collections import deque
+def dfs(graph, v, visited):
+    visited[v] = 1
+    result = [v]
+    for i in sorted(graph[v]):
+        if not visited[i]:
+            result.extend(dfs(graph, i, visited))
+    return result
 
-N, M, V = map(int, input().split())
+def bfs(graph, start, visited):
+    queue = [start]
+    visited[start] = 1
+    result = [start]
+    while queue:
+        v = queue.pop(0)
+        for i in sorted(graph[v]):
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = 1
+                result.append(i)
+    return result
 
-graph = [[0] * (N + 1) for _ in range(N + 1)]
+n, m, v = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
+    i, j = map(int, input().split())
+    graph[i].append(j)
+    graph[j].append(i)
 
-for _ in range(M):
-    a, b = map(int, input().split())
-    graph[a][b] = 1
-    graph[b][a] = 1
+visited = [0] * (n + 1)
+print(' '.join(map(str, dfs(graph, v, visited))))
 
-v1 = [0] * (N+1)
-v2 = [0] * (N+1)
-
-def bfs(V):
-    q = deque([V])
-    v2[V] = True
-    while q:
-        V = q.popleft()
-        print(V, end=" ")
-        for i in range(1, N + 1):
-            if not v2[i] and graph[V][i]:
-                q.append(i)
-                v2[i] = True
-
-def dfs(V):
-    v1[V] = 1
-    print(V, end=" ")
-    for i in range(1, N+1):
-        if not v1[i] and graph[V][i]:
-            dfs(i)
-
-dfs(V)
-print()
-bfs(V)
+visited = [0] * (n + 1)
+print(' '.join(map(str, bfs(graph, v, visited))))
