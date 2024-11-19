84e9bd7c0ebaf50add856db7b43cb6d4bbc0bd22
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2024-11-19 18:12:12 +0900

    [Silver III] Title: 병든 나이트, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1783. 병든 나이트/병든 나이트.py
+++ 백준/Silver/1783. 병든 나이트/병든 나이트.py
@@ -3,14 +3,12 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-if n == 1 :
-  print(1)
-
-elif n == 2 :
-  print(min((m + 1) // 2, 4))
-
-elif m < 7 :
-    print(min(m, 4))
-    
-else :
-    print(m - 2)
+if n == 1:
+    print(1)
+elif n == 2:
+    print(min(4, (m + 1) // 2))
+else:
+    if m <= 6:
+        print(min(4, m))
+    else:
+        print(m - 2)
