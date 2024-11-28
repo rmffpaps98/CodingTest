0e18264d47c7f2da14dad657ea763df8566c42c6
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2024-11-29 03:18:48 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 756 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -1,17 +1,18 @@
 import sys
 input = sys.stdin.readline
 
+n = int(input())
 
-def hanoi(n, start, auxiliary, end):
+
+def hanoi(n, start, via, end):
     if n == 1:
         print(start, end)
         return
 
-    hanoi(n - 1, start, end, auxiliary)
+    hanoi(n-1, start, end, via)
     print(start, end)
-    hanoi(n - 1, auxiliary, start, end)
+    hanoi(n-1, via, start, end)
 
 
-n = int(input())
 print(2 ** n - 1)
 hanoi(n, 1, 2, 3)
