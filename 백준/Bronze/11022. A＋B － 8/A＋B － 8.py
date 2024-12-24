28583f7170b2c1aaa48642d614a06f35366efa48
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2024-12-24 16:01:25 +0900

    [Bronze V] Title: A+B - 8, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -1,5 +1,8 @@
-T = int(input())
+import sys
+input = sys.stdin.readline
 
-for i in range(1, T+1) :
-    A, B = map(int, input().split())
-    print(f"Case #{i}: {A} + {B} =", A+B)
+t = int(input())
+
+for i in range(t):
+    a, b = map(int, input().split())
+    print(f"Case #{i+1}: {a} + {b} = {a+b}")
