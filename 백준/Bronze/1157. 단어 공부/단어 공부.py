a4269000060ed86cb16c0e75ec7a61e80f44d40f
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2024-12-26 18:47:47 +0900

    [Bronze I] Title: 단어 공부, Time: 72 ms, Memory: 34368 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,11 @@
+s = input().upper()
+ss = list(set(s))
+cnt = []
+
+for i in ss:
+    cnt.append(s.count(i))
+
+if cnt.count(max(cnt)) > 1:
+    print('?')
+else:
+    print(ss[cnt.index(max(cnt))])
