a16254f6b66fa99a7a7180bfe85675c6c8f5cba0
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2024-12-26 16:23:38 +0900

    [Bronze II] Title: 다이얼, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,22 @@
+d = input()
+t = 0
+
+for i in d:
+    if i in "ABC":
+        t += 3
+    elif i in "DEF":
+        t += 4
+    elif i in "GHI":
+        t += 5
+    elif i in "JKL":
+        t += 6
+    elif i in "MNO":
+        t += 7
+    elif i in "PQRS":
+        t += 8
+    elif i in "TUV":
+        t += 9
+    elif i in "WXYZ":
+        t += 10
+
+print(t)
